fa7a1dab1d09476035b4e6c14f1731fa039e01e4
Lars Kasper <kasper@biomed.ee.ethz.ch>
2013-04-10 18:49:12 +0000

    added pulse oxymetry detection for GE by Steffen Bollmann added manual cardiac peak detection by Jakob Heinzle
    git-svn-id: svn+ssh://colombo07.ethz.ch/home/kasperla/svn_repos/smoothing/trunk/tSNR_fMRI_SPM/CheckPhysRETROICOR/PhysIOToolbox@178 d7055aa0-e31f-11dd-b94e-b50442429228

---
+++ code/physio_correct_cardiac_pulses_manually.m
@@ -0,0 +1,111 @@
+function ons_secs = physio_correct_cardiac_pulses_manually(ons_secs,percentile,upperThresh,lowerThresh)
+
+% this fucntion takes the onsets from ECG measure and controls for
+% outliers (more or less than a threshold given by a percentile increased
+% or decreased by upperTresh or lowerThresh percent respectively.
+
+
+
+[outliersHigh,outliersLow,fh] = selectOutliers(ons_secs.t, ons_secs.cpulse, percentile, upperThresh, lowerThresh);
+if any(outliersHigh)
+    additionalPulse=[];
+    fh2=figure;
+    for outk=1:length(outliersHigh)
+        s=0;
+        while ~(s==1)
+            indStart = outliersHigh(outk)-1; indEnd = outliersHigh(outk)+2;
+            ind=find(ons_secs.t==ons_secs.cpulse(indStart))-100:find(ons_secs.t==ons_secs.cpulse(indEnd))+100;
+            figure(fh2); clf;
+            plot(ons_secs.t(ind),ons_secs.c(ind),'r')
+            hold on;
+            plot(ons_secs.cpulse(indStart:indEnd),ones(4,1)*max(ons_secs.c(ind)),'ok')
+            inpNum=input('How many triggers do you want to set? Enter a number between 0 and 10 : ');
+            I1=[];
+            for ii=1:inpNum
+                figure(fh2);
+                [I1(ii), J1] = ginput(1);
+                plot(I1(ii),J1, 'b*', 'MarkerSize',10);
+                
+            end
+            s=input('If you agree with the selected triggers, press 1 (then enter) : ');
+            if isempty(s)
+                s=0;
+            end
+        end
+        additionalPulse=[additionalPulse;I1'];
+    end
+    ons_secs.cpulse = sort([ons_secs.cpulse;additionalPulse]);
+    close(fh2);
+end
+close(fh);
+
+
+[outliersHigh,outliersLow,fh] = selectOutliers(ons_secs.t, ons_secs.cpulse, percentile, upperThresh, lowerThresh);
+finalIndex=1:length(ons_secs.cpulse);
+if any(outliersLow)
+    fh3=figure;
+    for outk=1:length(outliersLow)
+        s=0;
+        while ~(s==1)
+            indStart = outliersLow(outk)-2; indEnd = outliersLow(outk)+2;
+            ind=find(ons_secs.t==ons_secs.cpulse(indStart))-100:find(ons_secs.t==ons_secs.cpulse(indEnd))+100;
+            figure(fh3); clf;
+            plot(ons_secs.t(ind),ons_secs.c(ind),'r')
+            hold on;
+            plot(ons_secs.cpulse(indStart:indEnd),ones(5,1)*max(ons_secs.c(ind)),'ok','MarkerFaceColor','r');
+            alreadyDeleted=intersect(indStart:indEnd,setdiff(1:length(ons_secs.cpulse),finalIndex));
+            plot(ons_secs.cpulse(alreadyDeleted),ones(size(alreadyDeleted))*max(ons_secs.c(ind)),'or','filled');
+            for kk=indStart:indEnd
+                text(ons_secs.cpulse(kk),max(ons_secs.c(ind))*1.05,int2str(kk));
+            end
+            
+            delInd= [];
+            
+            delInd=input('Enter the number of a pulse you want to delete (0 if none): ');
+            plot(ons_secs.cpulse(delInd),max(ons_secs.c(ind)), 'rx', 'MarkerSize',20);
+            
+            s=input('If you agree with the deleted triggers, press 1 (then enter) : ');
+            if isempty(s)
+                s=0;
+            end
+            finalIndex=setdiff(finalIndex,delInd');
+        end
+        
+        close(fh3);
+    end
+    ons_secs.cpulse = sort(ons_secs.cpulse(finalIndex));
+end
+close(fh);
+
+
+end
+
+
+function [outliersHigh,outliersLow,fh] = selectOutliers(t,tCardiac,percentile,deviationPercentUp,deviationPercentDown)
+
+fh = get_default_fig_params();
+set(fh, 'Name','Diagnostics raw phys time series');
+dt = diff(tCardiac);
+
+plot(tCardiac(2:end), dt);
+xlabel('t (seconds)');
+ylabel('lag between heartbeats (seconds)');
+title('temporal lag between heartbeats');
+
+nBins = length(dt)/10;
+[dtSort,dtInd]=sort(dt);
+percentile=percentile/100;
+upperThresh=(1+deviationPercentUp/100)*dtSort(ceil(percentile*length(dtSort)));
+lowerThresh=(1-deviationPercentDown/100)*dtSort(ceil((1-percentile)*length(dtSort)));
+outliersHigh=dtInd(find(dtSort>upperThresh));
+outliersLow=dtInd(find(dtSort<lowerThresh));
+
+if nnz(dt > upperThresh)
+    text(t( find(dt==max(dt))+1 ),max(dt),...
+        {'Warning: There seem to be skipped heartbeats R-waves in the scanner-log', ...
+        sprintf('first at timepoint %01.1f s',tCardiac(min(outliersHigh+1))), ...
+        'rerun read\_physlog with ECG\_min set to 1'}, ...
+        'Color', [1 0 0])
+end
+
+end
--- code/physio_get_cardiac_pulses.m
+++ code/physio_get_cardiac_pulses.m
@@ -49,14 +49,15 @@ function cpulse= physio_get_cardiac_pulses(t, c, thresh_cardiac, verbose)
 %
 % $Id$
 
-%% detection of cardiac R-peaks via autocorrelation with 1 R-wave
+%% detection of cardiac R-peaks
 
+% debug=true;
+debug=false;
 switch lower(thresh_cardiac.modality)
-    case 'oxy'
-        
-        c = c-mean(c); c = c./max(c); % normalize time series
+case 'oxy'
+     c = c-mean(c); c = c./max(c); % normalize time series
         dt = t(2)-t(1);
-        dt120 = round(0.5/dt); % heart rate < 180 bpm
+        dt120 = round(0.5/dt); % heart rate < 120 bpm
         
         % smooth noisy pulse oximetry data to detect peaks
         w = gausswin(dt120,1);
@@ -69,8 +70,8 @@ switch lower(thresh_cardiac.modality)
         [b,a]=butter(forder,2/cutoff,'high');
         sc =filter(b,a, sc);
         sc = sc./max(sc);
-        
-        [tmp, cpulse] = physio_findpeaks(sc,'minpeakheight',thresh_cardiac.min,'minpeakdistance', dt120); 
+
+                [tmp, cpulse] = physio_findpeaks(sc,'minpeakheight',thresh_cardiac.min,'minpeakdistance', dt120); 
         
         if verbose >=2 % visualise influence of smoothing on peak detection
             fh = physio_get_default_fig_params;
@@ -90,6 +91,299 @@ switch lower(thresh_cardiac.modality)
         end
         
         cpulse = t(cpulse);
+
+        
+        case 'oxyGE'
+        c = c-mean(c); c = c./std(c); % normalize time series
+        dt = t(2)-t(1);
+        dt120 = round(0.5/dt); % heart rate < 120 bpm
+        
+        
+        % DEBUG
+        if debug
+            figure(1);clf;
+            subplot 211;
+            plot(t, c, 'k');
+            hold all;
+        end
+        % DEBUG
+        
+        %guess peaks in two steps with updated avereage heartrate
+        %first step
+        [tmp, cpulseFirstGuess] = findpeaks( ...
+            c,'minpeakheight',thresh_cardiac.min,'minpeakdistance', dt120);
+        
+        %second step
+        averageHeartRateInSamples = round(mean(diff(cpulseFirstGuess)));
+        [tmp, cpulseSecondGuess] = findpeaks(c,...
+            'minpeakheight',thresh_cardiac.min,...
+            'minpeakdistance', round(0.5*averageHeartRateInSamples));
+        
+        if debug
+%             hold on;
+%             stem(t(cpulseSecondGuess),4*ones(length(cpulseSecondGuess),1),'r')
+        end
+        
+        %test signal/detection quality
+        signalQualityIsBad = false;
+        
+        distanceBetweenPeaks = diff(cpulseSecondGuess);
+        
+        
+        nBins = length(distanceBetweenPeaks)/10;
+        [n,dtbin] = hist(distanceBetweenPeaks,nBins);
+        
+        percentile = 0.8;
+        deviationPercent = 60;
+        iPercentile = find(cumsum(n)>percentile*sum(n),1,'first');
+        if dtbin(end) > (1+deviationPercent/100)*dtbin(iPercentile)
+            signalQualityIsBad = true;
+        end
+        
+        %always use the correlation based method for testing
+%         signalQualityIsBad = true;
+       
+        if signalQualityIsBad
+            %build template based on the guessed peaks
+            halfTemplateWidthInSeconds = 0.2;
+            halfTempalteWithInSamples = halfTemplateWidthInSeconds / dt;
+            for n=2:numel(cpulseSecondGuess)-2
+                startTemplate=cpulseSecondGuess(n)-halfTempalteWithInSamples;
+                endTemplate=cpulseSecondGuess(n)+halfTempalteWithInSamples;
+                
+                template(n,:)= c(startTemplate:endTemplate);
+            end
+            
+            %delete first zero-elements of the template
+            template(1,:)=[];
+            
+            pulseTemplate=mean(template);
+            
+            % delete the peaks deviating from the mean too
+            % much before building the final template
+            for n=1:size(template,1)
+                correlation = corrcoef(template(n,:),pulseTemplate);
+                similarityToTemplate(n) = correlation(1,2);
+            end
+            
+            count = 1;
+            for n=1:size(template,1)
+                if similarityToTemplate(n) > 0.95
+                    cleanedTemplate(count,:)=template(n,:);
+                    count=count+1;
+                end
+            end
+            
+            if exist('cleanedTemplate','var')
+                pulseCleanedTemplate=mean(cleanedTemplate);
+                clear similarityToTemplate
+            else
+                pulseCleanedTemplate = mean(template);
+            end
+            
+            %determine starting peak for the search
+            forStart=2*halfTempalteWithInSamples+1;
+            forEnd=cpulseSecondGuess(20);
+            for n=forStart:forEnd
+                startSignalIndex=n-halfTempalteWithInSamples;
+                endSignalIndex=n+halfTempalteWithInSamples;
+                
+                signalPart = c(startSignalIndex:endSignalIndex);
+                correlation = corrcoef(signalPart,pulseCleanedTemplate);
+                
+                %Debug
+                %             figure(2);clf;
+                %             plot(signalPart);
+                %             hold all;
+                %             plot(pulseCleanedTemplate);
+                %Debug
+                
+                similarityToTemplate(n) = correlation(1,2);
+            end
+            
+            [C_bestMatch,I_bestMatch] = max(similarityToTemplate);
+            clear similarityToTemplate
+            
+            %now compute backwards to the beginning
+            n=I_bestMatch;
+            peakNumber = 1;
+            similarityToTemplate=zeros(size(t),1);
+            
+            searchStepsTotal=round(0.5*averageHeartRateInSamples);
+            while n > 1+searchStepsTotal+halfTempalteWithInSamples
+                for searchPosition=-searchStepsTotal:1:searchStepsTotal
+                    startSignalIndex=n-halfTempalteWithInSamples+searchPosition;
+                    endSignalIndex=n+halfTempalteWithInSamples+searchPosition;
+                    
+                    signalPart = c(startSignalIndex:endSignalIndex);
+                    correlation = corrcoef(signalPart,pulseCleanedTemplate);
+                    
+                    %DEBUG
+%                     if debug
+%                         figure(1);
+%                         subplot 212;
+%                         plot(signalPart);
+%                         hold all;
+%                         plot(pulseCleanedTemplate);
+%                         hold off;
+%                     end
+                    %DEBUG
+                    
+                    gaussianWindow = gausswin(2*searchStepsTotal+1);
+%                     currentWeight = gaussianWindow(searchPosition+searchStepsTotal+1);
+
+                    currentWeight = abs(c(n+searchPosition+1));
+                    correlationWeighted =  currentWeight .* correlation(1,2);
+                    similarityToTemplate(n+searchPosition) = correlationWeighted;
+                    
+                end
+                
+                %DEBUG
+                if debug
+                    if (n>100) && (n< size(c,1)-100)
+                        figure(1);subplot 211;plot(t,similarityToTemplate,'b-')
+                        xlim([t(n-100) t(n+100)])
+                    end
+                end
+                %DEBUG
+                
+                
+                %find biggest correlation-peak from the last search
+                indexSearchStart=n-searchStepsTotal;
+                indexSearchEnd=n+searchStepsTotal;
+                
+                indexSearchRange=indexSearchStart:indexSearchEnd;
+                searchRangeValues=similarityToTemplate(indexSearchRange);
+                [C_bestMatch,I_bestMatch] = max(searchRangeValues);
+                bestPosition = indexSearchRange(I_bestMatch);
+                
+                cpulse(peakNumber) = bestPosition;
+                peakNumber = peakNumber+1;
+                
+
+                n=bestPosition-averageHeartRateInSamples;
+            end
+
+            
+            n=bestPosition;
+            peakNumber=1;
+            clear cpulse;
+            %now correlate template with PPU signal at the positions
+            %where we would expect a peak based on the average heartrate and
+            %search in the neighborhood for the best peak, but weight the peaks
+            %deviating from the initial starting point by a gaussian
+            searchStepsTotal=round(0.5*averageHeartRateInSamples);
+            
+            if n< searchStepsTotal+halfTempalteWithInSamples+1
+                n=searchStepsTotal+halfTempalteWithInSamples+1;
+            end
+            
+            while n < size(c,1)-searchStepsTotal-halfTempalteWithInSamples
+                %search around peak
+                
+                for searchPosition=-searchStepsTotal:1:searchStepsTotal
+                    startSignalIndex=n-halfTempalteWithInSamples+searchPosition;
+                    endSignalIndex=n+halfTempalteWithInSamples+searchPosition;
+                    
+                    signalPart = c(startSignalIndex:endSignalIndex);
+                    correlation = corrcoef(signalPart,pulseCleanedTemplate);
+                    
+                    %DEBUG
+%                     if debug
+%                         figure(1);
+%                         subplot 212;
+%                         plot(signalPart);
+%                         hold all;
+%                         plot(pulseCleanedTemplate);
+%                         hold off;
+%                     end
+                    %DEBUG
+                    
+                    gaussianWindow = gausswin(2*searchStepsTotal+1);
+                    locationWeight = gaussianWindow(searchPosition+searchStepsTotal+1);
+%                     locationWeight = 1;
+                    amplitudeWeight = abs(c(n+searchPosition+1));
+%                     amplitudeWeight = 1;
+                    correlationWeighted =  locationWeight .* amplitudeWeight .* correlation(1,2);
+                    similarityToTemplate(n+searchPosition) = correlationWeighted;
+                    
+                    
+                end
+                
+                %DEBUG
+                if debug
+                    if (n>100) && (n< size(c,1)-100)
+                        figure(1);subplot 211;plot(t,similarityToTemplate,'b-')
+                        xlim([t(n-100) t(n+100)])
+                    end
+                end
+                %DEBUG
+                
+                
+                %find biggest correlation-peak from the last search
+                indexSearchStart=n-searchStepsTotal;
+                indexSearchEnd=n+searchStepsTotal;
+                
+                indexSearchRange=indexSearchStart:indexSearchEnd;
+                searchRangeValues=similarityToTemplate(indexSearchRange);
+                [C_bestMatch,I_bestMatch] = max(searchRangeValues);
+                bestPosition = indexSearchRange(I_bestMatch);
+                
+                if debug
+                    stem(t(bestPosition),4,'g');
+                end
+                
+                cpulse(peakNumber) = bestPosition;
+                peakNumber = peakNumber+1;
+                
+                %only take the last 20 cpulses to compute the current HeartRate
+                foundCpulses = size(cpulse,2);
+                
+                if  foundCpulses < 3
+                    currentHeartRateInSamples=averageHeartRateInSamples;
+                end
+                
+                if  (foundCpulses < 21) && (foundCpulses >= 3)
+                    currentHeartRateInSamples = round(mean(diff(cpulse)));
+                end
+                
+                if foundCpulses > 20
+                    currentCpulses = cpulse (foundCpulses-20:foundCpulses);
+                    currentHeartRateInSamples = round(mean(diff(currentCpulses)));
+                end
+                
+                
+                %check currentHeartRate
+                checkSmaller=currentHeartRateInSamples > 0.5*averageHeartRateInSamples;
+                checkLarger=currentHeartRateInSamples < 1.5*averageHeartRateInSamples;
+                
+                %jumpToNextPeakSearchArea
+                if (checkSmaller && checkLarger)
+                    n=bestPosition+currentHeartRateInSamples;
+                else
+                    n=bestPosition+averageHeartRateInSamples;
+                end
+            end
+        end
+        
+        if ~signalQualityIsBad
+            cpulse = cpulseSecondGuess;
+        end
+        
+        
+        if verbose >=2
+            fh = physio_get_default_fig_params();
+            set(fh, 'Name', 'PPU-OXY: Heart Beat Detection');
+            plot(t, c, 'k');
+            hold all;
+            stem(t(cpulse),4*ones(size(cpulse)), 'r');
+        end
+        
+        
+        cpulse = t(cpulse);
+        
+        
+        
     case 'ecg'
         do_manual_peakfind = thresh_cardiac.manual_peak_select;
         if do_manual_peakfind
