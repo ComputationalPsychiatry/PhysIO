d2de8a64112a9862b91671e2db0947b7c59d031c
Lars Kasper <kasper@biomed.ee.ethz.ch>
2014-02-15 14:24:27 +0000

    keywords
    git-svn-id: svn+ssh://colombo07.ethz.ch/home/kasperla/svn_repos/smoothing/trunk/tSNR_fMRI_SPM/CheckPhysRETROICOR/PhysIOToolbox@424 d7055aa0-e31f-11dd-b94e-b50442429228

--- code/tapas_physio_read_physlogfiles_custom.m
+++ code/tapas_physio_read_physlogfiles_custom.m
@@ -32,7 +32,7 @@ function [c, r, t, cpulse] = tapas_physio_read_physlogfiles_custom(log_files)
 % (either version 3 or, at your option, any later version). For further details, see the file
 % COPYING or <http://www.gnu.org/licenses/>.
 %
-% $Id: tapas_physio_read_physlogfiles_custom.m 354 2013-12-02 22:21:41Z kasperla $
+% $Id$
 
 %% read out values
 if ~isempty(log_files.respiration)
