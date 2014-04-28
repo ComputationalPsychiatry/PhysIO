41075bf283ecfe04d24851cd568ca052611b6314
Lars Kasper <kasper@biomed.ee.ethz.ch>
2014-04-28 12:39:28 +0000

    PhysIO: TODO list
    git-svn-id: svn+ssh://colombo07.ethz.ch/home/kasperla/svn_repos/smoothing/trunk/tSNR_fMRI_SPM/CheckPhysRETROICOR/PhysIOToolbox@472 d7055aa0-e31f-11dd-b94e-b50442429228

---
+++ TODO.m
@@ -0,0 +1,9 @@
+% Test Matlabbatch
+- decide whether output files are written in same directory or different one
+=> maybe output-dir as physio-parameter? (model? log_files? verbose?)
+- try out auto-peak detection; manual; post-hoc
+- write matlabbatch job-file for every of the 4 examples
+
+% Extension Matlabbatch
+- write toolbox menu => check in SnPM
+- plotting via spm_uitab
