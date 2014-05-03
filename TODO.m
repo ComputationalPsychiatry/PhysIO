a32ac8017700f4926451ff9be74c59cea3d0250a
Lars Kasper <kasper@biomed.ee.ethz.ch>
2014-05-03 08:59:17 +0000

    PhysIO: renamed examples, better header
    git-svn-id: svn+ssh://colombo07.ethz.ch/home/kasperla/svn_repos/smoothing/trunk/tSNR_fMRI_SPM/CheckPhysRETROICOR/PhysIOToolbox@486 d7055aa0-e31f-11dd-b94e-b50442429228

--- TODO.m
+++ TODO.m
@@ -1,8 +1,15 @@
+% HRV+RVT
+- downsample resp data before using auto-algorithm for volume computation
+
+% Warnings
+- warning with missed heartbeats more comprehensible: include warning with
+ percentage/count of missed heartbeats
+- remove warning about detrimental slices...
+
 % Test Matlabbatch
 - decide whether output files are written in same directory or different one
 => maybe output-dir as physio-parameter? (model? log_files? verbose?)
 - try out auto-peak detection; manual; post-hoc
-- write matlabbatch job-file for every of the 4 examples
 
 % Extension Matlabbatch
 - plotting via spm_uitab
