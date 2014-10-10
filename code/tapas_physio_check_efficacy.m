62fb7d4c08c7b27b16953c672c878003536027b8
Lars Kasper <kasper@biomed.ee.ethz.ch>
2014-10-10 12:02:13 +0000

    PhysIO: improved readability and general usability of check_efficacy
    git-svn-id: svn+ssh://colombo07.ethz.ch/home/kasperla/svn_repos/smoothing/trunk/PhysIOToolbox@540 d7055aa0-e31f-11dd-b94e-b50442429228

--- code/tapas_physio_check_efficacy.m
+++ code/tapas_physio_check_efficacy.m
@@ -16,24 +16,52 @@
 % START #MOD
 
 % general paths study
-pathSPM         = '~/code/matlab/spm12b';
-pathPhysIO      = '~/code/matlab/smoothing_trunk/tSNR_fMRI_SPM/CheckPhysRETROICOR/PhysIOToolbox/code';
-fileReport      = '~/PhysIOTest.ps'; % where contrast maps are saved
+pathSPM         = '~/Documents/code/matlab/spm12b';
+pathPhysIO      = '~/Documents/code/matlab/spm12b/toolbox/PhysIO';
+fileReport      = '~/Dropbox/Andreiuta/physio_rest_ioio_pharm/physio_IOIO_pharm/results/PhysIOTest.ps'; % where contrast maps are saved
 
 % logfile names sorted per session
 nSess = 1;
 
 % subject directories to be included into analysis
-dirData         = '';
-dirScans        = '/cluster/scratch_xl/shareholder/klaas/dandreea/IOIO/data';
-maskScans       = 'F_*';
-maskGLM         = '/signedPE';
-maskStructural  = 'spm_pre/struct/1_2';
+
+% root directory holding all subject directories
+pathDataRoot    = '/Users/kasperla/Dropbox/Andreiuta/physio_rest_ioio_pharm/physio_IOIO_pharm/glmAnalysis';
+
+% prefix of all subject directories
+maskSubjects    = 'DMPAD_*'; 
+
+% GLM analysis subdirectory of subject folder
+dirGLM          = ''; 
+
+% includes subdirectory of subject folder and file name mask
+maskStructural  = '^meanfunct_rest.nii';
+
+% names of physiological contrasts to be reported
+% namesPhysContrasts = {
+%             'All Phys Regressors'
+%             'Cardiac Regressors'
+%             'Respiratory Regressors'
+%             'Cardiac X Respiratory Interaction'
+%             'Movement Regressors'
+%             };
+
+namesPhysContrasts = {
+    'All Phys'
+    'Cardiac'
+    'Respiratory'
+    'Card X Resp Interation'
+    'Movement'
+    };
+
+
+physio          = tapas_physio_new('RETROICOR');
+model           = physio.model; % holding number of physiological regressors
 
 % END #MOD
 %% ========================================================================
 
-scans = dir(fullfile(dirScans,maskScans));
+scans = dir(fullfile(pathDataRoot,maskSubjects));
 scans = {scans.name};
 subjectIndices = 1:length(scans);
 
@@ -44,65 +72,60 @@ spm('defaults', 'fMRI');
 spm_jobman('initcfg');
 
 for s = subjectIndices
-
-    try
-    dirSubject = scans{s};
-    pathSubject = fullfile(dirScans,dirSubject); %dirSubject = scan
-    pathAnalysis = fullfile(dirScans,dirSubject,maskGLM);
-    fileSPM = fullfile(pathAnalysis, 'SPM.mat');
-    fileStruct = spm_select('FPList', fullfile(pathSubject, maskStructural), '^wm.*nii');
     
-    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Create and plot phys regressors F-contrasts
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    if exist(fileSPM, 'file')
-        load(fileSPM);
+    try
+        dirSubject = scans{s};
+        pathSubject = fullfile(pathDataRoot,dirSubject); %dirSubject = scan
+        pathAnalysis = fullfile(pathDataRoot,dirSubject,dirGLM);
+        fileSPM = fullfile(pathAnalysis, 'SPM.mat');
+        fileStruct = spm_select('FPList', fullfile(pathSubject, maskStructural));
         
-        % Do the physiological contrasts already exist
-        phys_cnames = {
-            'All Phys Regressors'
-            'Cardiac Regressors'
-            'Respiratory Regressors'
-            'Cardiac X Respiratory Interaction'
-            'Movement Regressors'
-            };
-        indC = zeros(5,1);
-        
-        matlabbatch = tapas_physio_check_prepare_job_contrasts(fileSPM, ...
-            SPM, pathPhysIO);
-        
-        % generate contrasts only if not already existing
-        for indPhysCon = 1:5
-            indC(indPhysCon) = tapas_physio_check_get_xcon_index(SPM, ...
-                phys_cnames{indPhysCon});
-        end
-        matlabbatch{1}.spm.stats.con.consess(find(indC)) = [];
-        if ~isempty(matlabbatch{1}.spm.stats.con.consess)
-            spm_jobman('run', matlabbatch);
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Create and plot phys regressors F-contrasts
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        if exist(fileSPM, 'file')
             load(fileSPM);
+            nContrasts = numel(namesPhysContrasts);
+            
+            
+            % Check whether contrasts already exist in SPM.mat
+            indContrasts = zeros(nContrasts,1);
+            for iContrast = 1:nContrasts
+                indContrasts(iContrast) = tapas_physio_check_get_xcon_index(SPM, ...
+                    namesPhysContrasts{iContrast});
+            end
+            
+            % generate contrasts only if not already existing
+            matlabbatch = tapas_physio_check_prepare_job_contrasts(fileSPM, ...
+                model, SPM, pathPhysIO);
+            matlabbatch{1}.spm.stats.con.consess(find(indContrasts)) = [];
+            if ~isempty(matlabbatch{1}.spm.stats.con.consess)
+                spm_jobman('run', matlabbatch);
+                load(fileSPM);
+            end
+            
+            % report contrasts
+            for iContrast = 1:nContrasts
+                indContrasts(iContrast) = tapas_physio_check_get_xcon_index(SPM, ...
+                    namesPhysContrasts{iContrast});
+                load(fullfile(pathPhysIO, 'tapas_physio_check_job_report'));
+                matlabbatch{1}.spm.stats.results.spmmat = cellstr(fileSPM);
+                matlabbatch{1}.spm.stats.results.conspec.titlestr = [dirSubject ' - ' namesPhysContrasts{iContrast}];
+                matlabbatch{1}.spm.stats.results.conspec.contrasts = indContrasts(iContrast);
+                spm_jobman('run', matlabbatch);                     % report result
+                %                     spm_print(fileReport)
+                spm_sections(xSPM,hReg, fileStruct);                % overlay structural
+                spm_mip_ui('Jump',spm_mip_ui('FindMIPax'),'glmax'); % goto global max
+                spm_print(fileReport)
+            end
+            
+            titstr = [dirSubject, ' - SPM.xX.X'];
+            title(regexprep(titstr,'_','\\_'));
+            set(gcf,'Name', titstr);
+            fprintf('good SPM: %s\n', dirSubject);
+        else % no file, report that
+            fprintf('no SPM.mat: %s\n', dirSubject);
         end
-        % report contrasts
-        for indPhysCon = 1:5
-            indC(indPhysCon) = tapas_physio_check_get_xcon_index(SPM, ...
-                phys_cnames{indPhysCon});
-            load(fullfile(pathPhysIO, 'tapas_physio_check_job_report'));
-            matlabbatch{1}.spm.stats.results.spmmat = cellstr(fileSPM);
-            matlabbatch{1}.spm.stats.results.conspec.titlestr = [dirSubject ' - ' phys_cnames{indPhysCon}];
-            matlabbatch{1}.spm.stats.results.conspec.contrasts = indC(indPhysCon);
-            spm_jobman('run', matlabbatch);                     % report result
-            %                     spm_print(fileReport)
-            spm_sections(xSPM,hReg, fileStruct);                % overlay structural
-            spm_mip_ui('Jump',spm_mip_ui('FindMIPax'),'glmax'); % goto global max
-            spm_print(fileReport)
-        end
-        
-        titstr = [dirSubject, ' - SPM.xX.X'];
-        title(regexprep(titstr,'_','\\_'));
-        set(gcf,'Name', titstr);
-        fprintf('good SPM: %s\n', dirSubject);
-    else % no file, report that
-        fprintf('no SPM.mat: %s\n', dirSubject);
-    end
     catch
         warning(sprintf('Subject ID %d: %s did not run through', s, dirSubject));
     end
