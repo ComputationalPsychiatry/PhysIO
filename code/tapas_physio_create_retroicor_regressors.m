113d6c87a6892b5b55068386c42e705e8c3e01e3
Lars Kasper <kasper@biomed.ee.ethz.ch>
2015-08-03 11:20:18 +0000

    PhysIO: nicer RETROICOR plot
    git-svn-id: svn+ssh://colombo07.ethz.ch/home/kasperla/svn_repos/smoothing/trunk/PhysIOToolbox@789 d7055aa0-e31f-11dd-b94e-b50442429228

--- code/tapas_physio_create_retroicor_regressors.m
+++ code/tapas_physio_create_retroicor_regressors.m
@@ -143,26 +143,9 @@ ons_secs.r_sample_phase =  tapas_physio_split_regressor_slices(...
 
 %% plot cardiac & resp. regressors
 if verbose.level >=2
-    verbose.fig_handles(end+1) = tapas_physio_get_default_fig_params();
+    R = [cardiac_sess, respire_sess, mult_sess];
     
-    set(gcf,'Name','RETROICOR timecourse physiological regressors');
-    if order.cr
-        Nsubs=3;
-        ax(3) = subplot(Nsubs,1,3);
-        plot([mult_sess+repmat(1:size(mult_sess,2),length(mult_sess),1)]);
-        xlabel('scan volumes');
-        title('RETROICOR multiplicative cardiac x respiratory regressors, vertical shift for visibility')
-    else
-        Nsubs=2;
-    end
-    ax(1) = subplot(Nsubs,1,1);
-    plot([cardiac_sess+repmat(1:size(cardiac_sess,2),length(cardiac_sess),1)]);
-    xlabel('scan volumes');
-    title('RETROICOR cardiac regressors, vertical shift for visibility')
+    verbose.fig_handles(end+1) = ...
+        tapas_physio_plot_retroicor_regressors(R, order);
     
-    ax(2) = subplot(Nsubs,1,2);
-    plot([respire_sess+repmat(1:size(respire_sess,2),length(respire_sess),1)]);
-    xlabel('scan volumes');
-    title('RETROICOR respiratory regressors, vertical shift for visibility')
-    if ~(isempty(cardiac_sess) || isempty(respire_sess)), linkaxes(ax,'x'); end
 end
