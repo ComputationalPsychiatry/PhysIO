d6a73820e02a91432568c80e8c5c5af3f1f818b5
Lars Kasper <kasper@biomed.ee.ethz.ch>
2018-02-17 10:34:44 +0100

    Added a function to check whether SPM batch editor integration of PhysIO works by searching for location of cfg_matlabbatch; useful for initialization PhysIO

---
+++ code/tapas_physio_check_spm_batch_editor_integration.m
@@ -0,0 +1,50 @@
+function isPhysioVisibleForSpmBatchEditor = ...
+    tapas_physio_check_spm_batch_editor_integration()
+% Checks whether PhysIO-configuration file for matlabbatch is in subfolder
+% of SPM/toolbox and returns a warning, if not
+%
+%   isPhysioVisibleForSpmBatchEditor = ...
+%       tapas_physio_check_spm_batch_editor_integration()
+%
+% IN
+%
+% OUT
+%
+% EXAMPLE
+%   tapas_physio_check_spm_batch_editor_integration
+%
+%   See also
+%
+% Author: Lars Kasper
+% Created: 2018-02-17
+% Copyright (C) 2018 TNU, Institute for Biomedical Engineering,
+%                    University of Zurich and ETH Zurich.
+%
+% This file is part of the TAPAS PhysIO Toolbox, which is released under the terms of the GNU General Public
+% License (GPL), version 3. You can redistribute it and/or modify it under the terms of the GPL
+% (either version 3 or, at your option, any later version). For further details, see the file
+% COPYING or <http://www.gnu.org/licenses/>.
+%
+
+if exist('spm') 
+    pathSpm = spm('Dir'); 
+else
+    warning('SPM is not on your Matlabpath. Please add it without its subfolders, e.g., via addpath');
+end
+
+% check for config matlabbatch file
+filePhysioCfgMatlabbatch = ...
+    dir(fullfile(pathSpm, 'toolbox', '**/tapas_physio_cfg_matlabbatch.m'));
+
+isPhysioVisibleForSpmBatchEditor = ~isempty(filePhysioCfgMatlabbatch);
+
+pathPhysIO = fileparts(mfilename('fullpath'));
+
+if ~isPhysioVisibleForSpmBatchEditor
+    warning(['\n The PhysIO Toolbox code folder has not been copied (or linked)' ...
+        ' to a subfolder of the SPM/toolbox folder. \n The Batch Editor will' ...
+        ' not show PhysIO. \n To make PhysIO visible there, link its path' ...
+        ' by typing the following in the Matlab command window (Linux/Mac) \n ' ...
+        ' and restart SPM afterwards:\n' ...
+        '       ln -s %s %s/toolbox/PhysIO'], pathPhysIO, pathSpm);
+end
