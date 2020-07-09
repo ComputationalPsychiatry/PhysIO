d6beab01d65e4c364f41ee1f70f73a215f5a4f0e
Lars Kasper <kasper@biomed.ee.ethz.ch>
2020-07-09 17:41:30 +0200

    Update tapas_physio_new.m; suggest 2 as c_pulse_select.min default for ECG in comments

--- code/tapas_physio_new.m
+++ code/tapas_physio_new.m
@@ -362,8 +362,9 @@ else
     preproc.cardiac.initial_cpulse_select.file = 'initial_cpulse_kRpeakfile.mat';
     
     % threshold for peak height in z-scored cardiac waveform to find pulse events
-    % May need increasing for ECG
     % default: 0.4
+    % NOTE: For ECG, might need increase (e.g., 2.0), because of local maximum 
+    %       of T wave after QRS complex
     preproc.cardiac.initial_cpulse_select.min = 0.4;
     
     % variable saving an example cardiac QRS-wave to correlate with
