a4d078b9010c051ee0f82adcf198b38a66530bdc
Johanna Bayer <johanna.bayer@orygen.org.au>
2023-04-25 13:07:21 +1000

    remove review initialize

--- code/preproc/tapas_physio_get_cardiac_pulses_auto_matched.m
+++ code/preproc/tapas_physio_get_cardiac_pulses_auto_matched.m
@@ -103,7 +103,6 @@ else
              cpulse, verbose);
 
     % freeze versions of t, c and cpulse for review
-    verbose.review =[];
     verbose.review.peak.t = t;
     verbose.review.peak.c = c;
     verbose.review.peak.cpulse = cpulse;
