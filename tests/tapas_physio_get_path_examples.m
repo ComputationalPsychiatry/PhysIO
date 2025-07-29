a87978da7ad257c11e7b5c54da969db91995aa67
Lars Kasper <Lars.Kasper@utoronto.ca>
2025-07-29 13:41:36 -0400

    Added tapas_physio_download_example_data function downloading examples from zenodo

--- tests/tapas_physio_get_path_examples.m
+++ tests/tapas_physio_get_path_examples.m
@@ -1,4 +1,5 @@
-function pathExamples = tapas_physio_get_path_examples(pathPhysioPublic)
+function pathExamples = tapas_physio_get_path_examples(pathPhysioPublic, ...
+    doVerifyPath)
 % Returns GitLab-internal or TAPAS-public PhysIO Examples folder, based on
 % location of public PhysIO directory
 %
@@ -7,12 +8,20 @@ function pathExamples = tapas_physio_get_path_examples(pathPhysioPublic)
 % IN
 %   pathPhysioPublic    location of public PhysIO folder, e.g.,
 %                       'tapas/PhysIO'
+%                       leave empty if location of PhysIO code should be
+%                       determined automatically
+%   doVerifyPath        true (1, [default]) or false
+%                       if true, a set of generic example paths are tested
+%                       and the first one that exists is returned
+%                       if false, the standard download path for examples,
+%                       as used by tapas_physio_download_example_data(), is
+%                       returned
 % OUT
 %
 % EXAMPLE
 %   tapas_physio_get_path_examples('tapas/PhysIO')
 %
-%   See also
+%   See also tapas_physio_download_example_data
 
 % Author:   Lars Kasper
 % Created:  2022-09-05
@@ -25,34 +34,46 @@ function pathExamples = tapas_physio_get_path_examples(pathPhysioPublic)
 % version 3 or, at your option, any later version). For further details,
 % see the file COPYING or <http://www.gnu.org/licenses/>.
 
-if nargin < 1
-    pathPhysioPublic = fullfile(fileparts(mfilename('fullpath')), '..');
+if nargin < 1 || isempty(pathPhysioPublic)
+    pathPhysioPublic = tapas_physio_simplify_path(fullfile(fileparts(mfilename('fullpath')), '..'));
 end
-    
-% try PhysIO-internal GitLab examples first
+
+if nargin < 2
+    doVerifyPath = true;
+end
+
+% try PhysIO examples from Zenodo first
+% then: old separate Gitlab repository via submodule in PhysIO Gitlab
+% then: old separate Gitlab repository as separate repository
 possibleExamplePaths = {
-    fullfile(pathPhysioPublic, '..', 'examples')    
+    fullfile(pathPhysioPublic, 'examples')
+    fullfile(pathPhysioPublic, '..', 'examples')
     fullfile(pathPhysioPublic, '..', 'physio-examples')
-};
+    };
 
-iPath = 0;
-haveFoundExamplePath = false;
-while ~haveFoundExamplePath && iPath < numel(possibleExamplePaths)
-    iPath = iPath + 1;
-    pathExamples = possibleExamplePaths{iPath};
-    haveFoundExamplePath = isfolder(fullfile(pathExamples, 'BIDS'));
-end
+if doVerifyPath
+    iPath = 0;
+    haveFoundExamplePath = false;
+    while ~haveFoundExamplePath && iPath < numel(possibleExamplePaths)
+        iPath = iPath + 1;
+        pathExamples = possibleExamplePaths{iPath};
+        haveFoundExamplePath = isfolder(fullfile(pathExamples, 'BIDS'));
+    end
 
-% otherwise use public TAPAS examples
-if ~haveFoundExamplePath
-    pathExamples =  fullfile(pathPhysioPublic, ...
-        '..', 'examples', tapas_get_current_version(), 'PhysIO');
+    % otherwise use public TAPAS examples
+    if ~haveFoundExamplePath
+        pathExamples =  fullfile(pathPhysioPublic, ...
+            '..', 'examples', tapas_get_current_version(), 'PhysIO');
+    end
+
+    % If no examples folder found, suggest to download them via tapas-function
+    if ~isfolder(fullfile(pathExamples, 'BIDS'))
+        physio = tapas_physio_new();
+        tapas_physio_log('No PhysIO examples data found. Please download via tapas_physio_download_example_data()', physio.verbose, 2);
+    end
+
+else % use canonical zenodo download path for examples
+    pathExamples = possibleExamplePaths{1};
 end
 
 pathExamples = tapas_physio_simplify_path(pathExamples);
-
-% If no examples folder found, suggest to download them via tapas-function
-if ~isfolder(fullfile(pathExamples, 'BIDS'))
-    physio = tapas_physio_new();
-    tapas_physio_log('No PhysIO examples data found. Please download via tapas_download_example_data()', physio.verbose, 2);
-end
